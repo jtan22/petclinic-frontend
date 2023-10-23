d147b014cb32d8dba6a0ec3148f9fb6f4cd169af
Jason Tan <jtan20040401@gmail.com>
2023-10-23 11:06:17 +1100

    Added error page and test crash controller

---
+++ src/main/java/com/bw/petclinic/frontend/controller/CrashController.java
@@ -0,0 +1,19 @@
+package com.bw.petclinic.frontend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CrashController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CrashController.class);
+
+    @GetMapping("/oops")
+    public String error() {
+        LOG.info("Get /oops");
+        throw new RuntimeException("Expected Exception");
+    }
+
+}
