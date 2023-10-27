38a0ad199d136b16d99267bfc7ed3d9bdf513823
Jason Tan <jtan20040401@gmail.com>
2023-10-27 14:50:57 +1100

    Use Hateoas PagedModel to get Vets, instead of CustomPageImpl

--- src/main/java/com/bw/petclinic/frontend/service/VetServiceImpl.java
+++ src/main/java/com/bw/petclinic/frontend/service/VetServiceImpl.java
@@ -1,21 +1,26 @@
 package com.bw.petclinic.frontend.service;
 
 import com.bw.petclinic.frontend.domain.Vet;
-import com.bw.petclinic.frontend.domain.CustomPageImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 @Primary
 public class VetServiceImpl implements VetService {
 
-    private static final String GET_PAGED_VETS = "/vets?pageNumber=%d&pageSize=%d";
+    private static final String GET_HAL_PAGED_VETS = "/data-rest/vets?page=%d&size=%d";
 
     @Value("${rest.service.vets}")
     private String restServiceVets;
@@ -28,9 +33,16 @@ public class VetServiceImpl implements VetService {
     }
 
     public Page<Vet> getVets(int pageNumber, int pageSize) {
-        String uri = String.format(restServiceVets + GET_PAGED_VETS, pageNumber, pageSize);
-        return restTemplate.exchange(uri, HttpMethod.GET, null,
-                new ParameterizedTypeReference<CustomPageImpl<Vet>>() {}).getBody();
+        String halUri = String.format(restServiceVets + GET_HAL_PAGED_VETS, pageNumber, pageSize);
+        PagedModel<Vet> pagedVets = restTemplate.exchange(halUri, HttpMethod.GET, null,
+                new ParameterizedTypeReference<PagedModel<Vet>>() {}).getBody();
+        if (pagedVets == null || pagedVets.getMetadata() == null) {
+            throw new RuntimeException("Get Vets [" + pageNumber + "/" + pageSize + "] not found");
+        }
+        List<Vet> vets = List.copyOf(pagedVets.getContent());
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        long totalElements = pagedVets.getMetadata().getTotalElements();
+        return new PageImpl<>(vets, pageable, totalElements);
     }
 
 }
